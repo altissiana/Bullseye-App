fbedcc8d5df65a5f186975dd44a7c830ff32dbad
Tissiana Alves <altissiana@gmail.com>
2020-06-17 15:02:49 -0600

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Tissiana Alves on 6/17/20.
+//  Copyright © 2020 Tissiana Alves. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
